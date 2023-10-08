c6ec70dea67eb14ab42e85b7c641641b8970e716
annarusanova <anna.y.rusanova@yandex.ru>
2023-10-08 18:36:31 +0300

    Internationalization

---
+++ www/js/lang/en.js
@@ -0,0 +1,19 @@
+const en_lang = {
+    "t_distance": "Distance",
+    "t_400_m": "400 m.",
+    "t_800_m": "800 m.",
+    "t_1000_m": "1000 m.",
+    "t_3000_m": "3000 m.",
+    "t_5_km": "5 km.",
+    "t_10_km": "10 km.",
+    "t_half_marathon": "Half-marathon",
+    "t_marathon": "Marathon",
+    "t_custom": "Custom",
+    "t_pace_min_km": "Pace (min/km)",
+    "t_time_h_m_s": "Time (h:m:s)",
+    "t_distance_m": "Distance, m.",
+    "t_time": "Time",
+    "t_h": "h.",
+    "t_m": "m.",
+    "t_s": "s."
+}
